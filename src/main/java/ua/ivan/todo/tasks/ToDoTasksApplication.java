4a1b893dd976211a22a96bb87364c2db197a56e0
LainIsHDEATH <ivan.shankin04@gmail.com>
2026-06-09 16:11:41 +0300

    Initial commit.

---
+++ src/main/java/ua/ivan/todo/tasks/ToDoTasksApplication.java
@@ -0,0 +1,13 @@
+package ua.ivan.todo.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoTasksApplication.class, args);
+    }
+
+}
